7b2248fde6b4c8d2ee136c2497ce907d41502a20
AnasKAN <anasaldadi@gmail.com>
2024-01-26 21:19:09 +0300

    adding home controller

---
+++ HomeController.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class HomeController{
+
+    private Scene scene;
+    private Stage stage;
+
+    @FXML
+    private Label userLbl;
+
+    public void displayname(String username){
+        userLbl.setText(username);
+    }
+
+    public void logout(ActionEvent e) throws IOException{
+        System.out.println("hello");
+        Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+        stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switch2sceneQuiz(ActionEvent e) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("Quiz.fxml"));
+        stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switch2sceneQuestion(ActionEvent e) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("Question.fxml"));
+        stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}

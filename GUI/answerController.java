2b7df94d03a2191b2044972dc3b028f13c7708de
FS-Baz <s443011919@st.uqu.edu.sa>
2024-01-31 17:50:08 +0300

    Answer screen with controller

---
+++ GUI/answerController.java
@@ -0,0 +1,56 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class answerController {
+
+    @FXML
+    private Button answerBtn1;
+
+    @FXML
+    private Button answerBtn2;
+
+    @FXML
+    private Button answerBtn3;
+
+    @FXML
+    private Button answerBtn4;
+
+    @FXML
+    private Button gradesbtn;
+
+    @FXML
+    private Button hintBtn;
+
+    @FXML
+    private Button homeBtn;
+
+    @FXML
+    private Button logoutbtn;
+
+    @FXML
+    private Text qNumber;
+
+    @FXML
+    private Text question;
+
+    @FXML
+    private Button quizzesbtn;
+
+    @FXML
+    private Text timerTxt;
+
+    @FXML
+    private Label userLbl;
+
+    @FXML
+    private Label welcomeLbl;
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+}

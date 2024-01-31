1c3fa221c635391dc01999e33b0b686cb54557e7
FS-Baz <s443011919@st.uqu.edu.sa>
2024-01-31 18:11:58 +0300

    Grades screen and controller

---
+++ GUI/GradesController.java
@@ -0,0 +1,86 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class GradesController {
+
+    @FXML
+    private Text gradeTxt1;
+
+    @FXML
+    private Text gradeTxt2;
+
+    @FXML
+    private Text gradeTxt3;
+
+    @FXML
+    private Text gradeTxt4;
+
+    @FXML
+    private Text gradeTxt5;
+
+    @FXML
+    private Text gradeTxt6;
+
+    @FXML
+    private Button gradesbtn;
+
+    @FXML
+    private Button homeBtn;
+
+    @FXML
+    private Button logoutbtn;
+
+    @FXML
+    private Text quizNameTxt1;
+
+    @FXML
+    private Text quizNameTxt2;
+
+    @FXML
+    private Text quizNameTxt3;
+
+    @FXML
+    private Text quizNameTxt4;
+
+    @FXML
+    private Text quizNameTxt5;
+
+    @FXML
+    private Text quizNameTxt6;
+
+    @FXML
+    private Button quizzesbtn;
+
+    @FXML
+    private Text subjectTxt1;
+
+    @FXML
+    private Text subjectTxt2;
+
+    @FXML
+    private Text subjectTxt3;
+
+    @FXML
+    private Text subjectTxt4;
+
+    @FXML
+    private Text subjectTxt5;
+
+    @FXML
+    private Text subjectTxt6;
+
+    @FXML
+    private Label userLbl;
+
+    @FXML
+    private Label welcomeLbl;
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+}
